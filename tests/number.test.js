7187f6926096218f2d3b72223509af4508e4d4de
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-01-28 00:51:39 +0100

    feat numbers: basic utest - if value is num - if value is integer - if value is float - if value is between indicated limits

---
+++ tests/number.test.js
@@ -0,0 +1,45 @@
+import { number, integer, float, between  } from './number.js';
+
+test('number if value is null', () => {
+    expect(required(null)).toBe(false);
+});
+
+test('number if value is undefined', () => {
+    expect(required(undefined)).toBe(false);
+});
+
+test('number if value is a number', () => {
+    expect(number(1)).toBe(true);
+});
+
+test('number if value is a string', () => {
+    expect(number('1')).toBe(false);
+});
+
+test('integer if value is a number', () => {
+    expect(integer(1)).toBe(true);
+});
+
+test('integer if value is a float', () => {
+    expect(integer(1.1)).toBe(false);
+});
+
+test('float if value is a number', () => {
+    expect(float(1)).toBe(false);
+});
+
+test('float if value is a float', () => {
+    expect(float(1.1)).toBe(true);
+});
+
+test('between if value is between the limits', () => {
+    expect(between(5, 1, 10)).toBe(true);
+});
+
+test('between if value is below the limits', () => {
+    expect(between(0, 1, 10)).toBe(false);
+});
+
+test('between if value is above the limits', () => {
+    expect(between(11, 1, 10)).toBe(false);
+});
