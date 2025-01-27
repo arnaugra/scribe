a5da0e0d6e745e3a15f90b9cd83fdc251a171e40
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-01-28 00:03:28 +0100

    feat index: base of the Scribe validator class

---
+++ src/index.js
@@ -0,0 +1,36 @@
+/**
+ * Scribe - Class:
+ * Validate data following a set of rules
+ * 
+ * Methods:
+ * - validate(rules): Validate the data following the rules given by array
+ * - passes(): Verify if the data passes the validation
+ * - getErrors(): Get the errors of the validation
+ */
+export default class Scribe {
+    constructor(data) {
+        this.data = data;
+        this.errors = {};
+    }
+
+    validate(rules) {
+        Object.keys(rules).forEach(field => {
+            const rulesArray = rules[field];
+
+            rulesArray.forEach(rule => {
+                // extract rule name and param (if exists)
+                const [ruleName, param] = rule.split(':');
+                
+                // call the rule function
+            });
+        });
+    }
+
+    passes() {
+        return Object.keys(this.errors).length === 0;
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+}
