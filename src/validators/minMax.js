0e7b47b793760dee32cfc57aa3b07b65d1509975
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-01-29 02:30:43 +0100

    feat minMax: nullable case added

--- src/validators/minMax.js
+++ src/validators/minMax.js
@@ -1,12 +1,17 @@
+import { nullableRule } from "./required";
+
 /**
  * Verify that max length of a value is less than a number
  * @example { age: ["max:18"] }
  * @param { String | Number } value Value to validate
- * @param { Number } max Max length of the value
+ * @param { ...args } max Max length of the value
+ * @param { ...args } nullable Nullable value
  * @returns { Boolean } True if the value is less than the limit
  */
-export function max(value, max) {
-    if( min === null || min === undefined) return false;
+export function max(value, ...args) {
+    const [max, nullableValue] = args;
+    if (nullableRule(value, nullableValue)) return true;
+    if (max === null || max === undefined) return false;
     if (typeof value === 'number') return value <= max;
     return value.length <= max;
 }
@@ -15,11 +20,14 @@ export function max(value, max) {
  * Verify that min length of a value is greater than a number
  * @example { age: ["max:18"] }
  * @param { String | Number } value Value to validate
- * @param { Number } min Min length of the value
+ * @param { ...args } min Min length of the value
+ * @param { ...args } nullable Nullable value
  * @returns { Boolean } True if the value is greater than the limit
  */
-export function min(value, min) {
-    if( min === null || min === undefined) return false;
+export function min(value, ...args) {
+    const [min, nullableValue] = args;
+    if (nullableRule(value, nullableValue)) return true;
+    if (min === null || min === undefined) return false;
     if (typeof value === 'number') return value >= min;
     return value.length >= min;
 }
